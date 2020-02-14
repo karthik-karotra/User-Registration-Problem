1e051d4f6f44893dc56630e5e2b1f5064831dc6e
karthik-karotra <karthikpatel54@gmail.com>
2020-02-14 19:03:41 +0530

    ADDED:Validation for password length

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -5,6 +5,7 @@ read -p "Enter first name: " firstName
 read -p "Enter last name: " lastName
 read -p "Enter email id: " email
 read -p "Enter phone number: " phone
+read -p "Enter password" password
 function validateUser (){
 	if [[ $1 =~ $2 ]]
 	then
@@ -16,11 +17,12 @@ function validateUser (){
 validNamePattern="^[A-Z]{1}[a-z]{2,}$"
 validEmailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
 validPhoneNoPattern="^[0-9]{1,3}[[:space:]][0-9]{10}$"
+validPasswordPattern="^[a-zA-Z0-9]{8,}"
 validateUser "$firstName" $validNamePattern
 validateUser "$lastName" $validNamePattern
 validateUser "$email" $validEmailPattern
 validateUser "$phone" $validPhoneNoPattern
-
+validateUser "$password" $validPasswordPattern
 
 
 
