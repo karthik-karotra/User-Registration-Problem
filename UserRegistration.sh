e14524fd5d2463b59834ea8f41c7bff6c55a767a
karthik-karotra <karthikpatel54@gmail.com>
2020-02-14 18:27:43 +0530

    ADDED:Validation of email of user

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,14 +3,17 @@ shopt -s extglob
 echo "Welcome to User Registration Desk"
 read -p "Enter first name: " firstName
 read -p "Enter last name: " lastName
+read -p "Enter email id: " email
 function validateUser (){
 	if [[ $1 =~ $2 ]]
 	then
-		echo "Valid Name"
+		echo "Valid Details"
 	else
 		echo "Invalid"
 	fi
 }
 validNamePattern="^[A-Z]{1}[a-z]{2,}$"
-validateUser $firstName $validNamePattern
-validateUser $lastName $validNamePattern
+validEmailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
+validateUser "$firstName" $validNamePattern
+validateUser "$lastName" $validNamePattern
+validateUser "$email" $validEmailPattern
