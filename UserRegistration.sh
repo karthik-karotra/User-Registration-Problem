6767ffaa1ddc76ac60da72d2aba46b2e9ed8decc
karthik-karotra <karthikpatel54@gmail.com>
2020-02-14 17:45:01 +0530

    ADDED:Validation for first name of user

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "Welcome to User Registration Desk"
+read -p "Enter first name: " firstName
+
+function validateUser (){
+	if [[ $1 =~ $2 ]]
+	then
+		echo "Valid First Name"
+	else
+		echo "Invalid"
+	fi
+}
+validNamePattern="^[A-Z]{1}[a-z]{2,}$"
+validateUser $firstName $validNamePattern
