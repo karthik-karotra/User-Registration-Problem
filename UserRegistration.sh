1df62e0970a33ac221bc9b46367e889997fa044b
karthik-karotra <karthikpatel54@gmail.com>
2020-02-14 18:40:15 +0530

    ADDED:Validation for user's phone number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,6 +4,7 @@ echo "Welcome to User Registration Desk"
 read -p "Enter first name: " firstName
 read -p "Enter last name: " lastName
 read -p "Enter email id: " email
+read -p "Enter phone number: " phone
 function validateUser (){
 	if [[ $1 =~ $2 ]]
 	then
@@ -14,6 +15,13 @@ function validateUser (){
 }
 validNamePattern="^[A-Z]{1}[a-z]{2,}$"
 validEmailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
+validPhoneNoPattern="^[0-9]{1,3}[[:space:]][0-9]{10}$"
 validateUser "$firstName" $validNamePattern
 validateUser "$lastName" $validNamePattern
 validateUser "$email" $validEmailPattern
+validateUser "$phone" $validPhoneNoPattern
+
+
+
+
+
