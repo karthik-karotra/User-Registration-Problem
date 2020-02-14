ff4098af976a2c3e40dabf2167af80e2a5078949
karthik-karotra <karthikpatel54@gmail.com>
2020-02-14 17:52:13 +0530

    ADDED:Validation for last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 shopt -s extglob
 echo "Welcome to User Registration Desk"
 read -p "Enter first name: " firstName
-
+read -p "Enter last name: " lastName
 function validateUser (){
 	if [[ $1 =~ $2 ]]
 	then
-		echo "Valid First Name"
+		echo "Valid Name"
 	else
 		echo "Invalid"
 	fi
 }
 validNamePattern="^[A-Z]{1}[a-z]{2,}$"
 validateUser $firstName $validNamePattern
+validateUser $lastName $validNamePattern
